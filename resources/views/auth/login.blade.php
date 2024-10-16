b3c23a09e7d320a5fa879379f2625decc91266c6
Umesh Rana <umesh@Umeshs-MacBook-Pro.local>
2024-10-16 14:04:33 +0530

    Services updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,7 +36,6 @@
 
             {{-- Login with Google --}}
             <a href="{{ route('auth.google') }}" class="inline-block px-6 py-2 text-white bg-red-600 rounded-lg shadow">Login with Google</a>
-            <a href="{{ route('auth.facebook') }}" class="inline-block px-6 py-2 text-white bg-blue-600 rounded-lg shadow">Login with FB</a>
 
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
