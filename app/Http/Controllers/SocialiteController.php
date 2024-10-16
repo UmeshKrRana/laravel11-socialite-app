626b00e5e16a50ae6f040696cae700407498fec7
Umesh Rana <umesh@Umeshs-MacBook-Pro.local>
2024-10-16 13:44:58 +0530

    Social login setup and Google login functionality added

---
+++ app/Http/Controllers/SocialiteController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class SocialiteController extends Controller
+{
+    /**
+     * Function: googleLogin
+     * Description: This function will redirect to Google
+     * @param NA
+     * @return void
+     */
+    public function googleLogin() {
+        return Socialite::driver('google')->redirect();
+    }
+
+    /**
+     * Function: googleAuthentication
+     * Decription: This function will authenticate the user through the Google Account
+     * @param NA
+     * @return void
+     */
+    public function googleAuthentication() {
+        try {
+            $googleUser = Socialite::driver('google')->user();
+
+            $user = User::where('google_id', $googleUser->id)->first();
+
+            if ($user) {
+                Auth::login($user);
+                return redirect()->route('dashboard');
+            } else {
+                $userData = User::create([
+                    'name' => $googleUser->name,
+                    'email' => $googleUser->email,
+                    'password' => Hash::make('Password@1234'),
+                    'google_id' => $googleUser->id,
+                ]);
+
+                if ($userData) {
+                    Auth::login($userData);
+                    return redirect()->route('dashboard');
+                }
+            }
+        } catch (Exception $e) {
+            dd($e);
+        }
+    }
+}
